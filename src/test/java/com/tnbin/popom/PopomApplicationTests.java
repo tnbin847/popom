eac30c80c47868daa0394c15149549640e803798
tnbin847 <tnqls3718@gmail.com>
2024-10-03 14:23:17 +0900

    Initial commit

---
+++ src/test/java/com/tnbin/popom/PopomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnbin.popom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
